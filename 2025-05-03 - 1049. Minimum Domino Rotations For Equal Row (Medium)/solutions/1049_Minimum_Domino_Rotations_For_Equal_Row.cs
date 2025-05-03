9a5edf7891ead229dc12046b58d21f6624aa9e17
runner <runner@fv-az1314-733>
2025-05-03 04:13:55 +0000

    Add solution for 1049. Minimum Domino Rotations For Equal Row

---
+++ 2025-05-03 - 1049. Minimum Domino Rotations For Equal Row (Medium)/solutions/1049_Minimum_Domino_Rotations_For_Equal_Row.cs
@@ -0,0 +1,8 @@
+# Minimum Domino Rotations For Equal Row - C# Solution
+# Problem URL: https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/
+
+public class Solution {
+    public int MinDominoRotations(int[] tops, int[] bottoms) {
+        
+    }
+}
