43382f0114d72152eec6c861ba4c8ecb9d618419
Prashant Mishra <2k23.csaiml2310693@gmail.com>
2025-05-03 09:49:55 +0530

    Delete 2025-05-03 - 1049. Minimum Domino Rotations For Equal Row (Medium) directory

--- 2025-05-03 - 1049. Minimum Domino Rotations For Equal Row (Medium)/solutions/1049_Minimum_Domino_Rotations_For_Equal_Row.ts
+++
@@ -1,6 +0,0 @@
-# Minimum Domino Rotations For Equal Row - TypeScript Solution
-# Problem URL: https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/
-
-function minDominoRotations(tops: number[], bottoms: number[]): number {
-    
-};
